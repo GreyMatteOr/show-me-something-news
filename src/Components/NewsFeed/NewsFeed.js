bb8ea8ffb826c59d53c3fb619bb8965922d4a545
Matthew Lane <themplane@gmail.com>
2020-11-05 21:22:02 -0800

    Create Base NewsFeed Component.

---
+++ src/Components/NewsFeed/NewsFeed.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class NewsFeed extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
+}
+
+export default NewsFeed;
