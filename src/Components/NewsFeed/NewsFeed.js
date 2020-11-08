895aa0989992aeaa34f8d26a90af31245569196d
Matthew Lane <themplane@gmail.com>
2020-11-08 01:13:13 -0800

    Implement Change page functionality.

--- src/Components/NewsFeed/NewsFeed.js
+++ src/Components/NewsFeed/NewsFeed.js
@@ -1,4 +1,6 @@
+import loading from '../../images/loading.png';
 import NewsCard from '../NewsCard/NewsCard.js';
+import './NewsFeed.css';
 import React, { Component } from 'react';
 import { timesAPI } from '../../api-requests.js';
 
@@ -7,25 +9,61 @@ class NewsFeed extends Component {
     super(props);
     this.state = {
       articles: [],
-      currentPage: null,
       isError: false,
       isLoading: true,
-      pageMax: null
+      loadingOverlayState: 'hidden',
+      pageMax: 1
     }
   }
 
   componentDidMount() {
-    timesAPI.getArticles('Apple Banana Kiwi')
+    this.changePage({target:{value:0}});
+  }
+
+  changePage = (e) => {
+    this.setState( {loadingOverlayState: 'shown'} )
+    timesAPI.getArticles('Apple Banana Kiwi', e.target.value)
     .then( data => {
-      console.log(data)
       if (data === 'error') {
         return this.setState( {isError: true} )
       }
       let newState = {isLoading: false};
       newState.articles = data.response.docs;
+      newState.pageMax = Math.min( (data.response.meta.hits / 10), 100);
+      newState.loadingOverlayState = 'hidden';
       this.setState(newState);
     })
     .catch( () => this.setState( {isError: true} ))
+  }
+
+  NewsCards = () => {
+    let uniqueArticles = this.state.articles.reduce( (unique, article ) => {
+      let isNew = unique.every( inside => inside.web_url !== article.web_url );
+      if (isNew) unique.push(article);
+      return unique;
+    }, []);
+
+    return uniqueArticles.map( (article, i) => {
+      return <NewsCard key={i} article={article} />
+    });
+  }
+
+  PageSelect = () => {
+    let optionsHTML = [];
+    while (optionsHTML.length < this.state.pageMax) {
+      let pos = optionsHTML.length;
+      optionsHTML.push(<option key={pos} value={pos}>{pos + 1}</option>)
+    }
+
+    return (
+      <div className='page-selection'>
+        current page
+        <select
+          onChange={this.changePage}>
+          {optionsHTML}
+        </select>
+      </div>
+    )
   }
 
   render() {
@@ -36,14 +74,19 @@ class NewsFeed extends Component {
       return <h3>Loading Articles...</h3>
     }
 
-    let newscards = this.state.articles.reduce( (unique, article ) => {
-      if (unique.every( inside => inside.web_url !== article.web_url)) unique.push(article);
-      return unique;
-    }, [])
-    .map( (article, i) => {
-      return <NewsCard key={i} article={article} />
-    })
-    return newscards
+    return (
+      <>
+        <div className={this.state.loadingOverlayState}>
+          <img
+            className='spin'
+            src={loading}
+            alt='Loading Content'
+          />
+        </div>
+        <this.PageSelect />
+        <this.NewsCards />
+      </>
+    )
   }
 }
 
