b10cd0d003e58e34c8496e2ee9e3ef9e32fb23fb
Matthew Lane <themplane@gmail.com>
2020-11-07 23:40:00 -0800

    Created tests to Validate NewsCard.

--- src/Components/NewsCard/NewsCard.js
+++ src/Components/NewsCard/NewsCard.js
@@ -3,30 +3,31 @@ import newspaper from '../../images/newspaper.png';
 import './NewsCard.css';
 
 function NewsCard({ article } ) {
+
   function makeImage() {
-    let image = article.multimedia.find( media => media.type === 'image' && media.width > 300)
-    if (image === undefined) {
-      return (
-        <img
-          src={newspaper}
-          alt={`"${article.headline.main}"`}
-        />
-      )
-    }
+    let image = article.multimedia.find( media => media.type === 'image' && media.width >= 160);
+    let src = 'https://www.nytimes.com/';
+    if (image === undefined) src = newspaper;
+    else src += image.url
     return (
       <img
-        src={'https://www.nytimes.com/' + image.url}
+        data-testid='nc-image'
+        src={src}
         alt={`"${article.headline.main}"`}
       />
     )
   }
 
+  function getBestSummary() {
+    let summary = (article.abstract.length > article.lead_paragraph.length
+      ? article.abstract
+      : article.lead_paragraph
+    );
+    if (summary.length === 0) summary = `${article.type_of_material}: ${article.headline.main}`;
+    return summary;
+  }
 
-  let summary = (article.abstract.length > article.lead_paragraph.length
-    ? article.abstract
-    : article.lead_paragraph
-  );
-  if (summary.length === 0) summary = `${article.type_of_material}: ${article.headline.main}`;
+
   return (
     <Link to={article.web_url}
       className='news-card'
@@ -34,14 +35,14 @@ function NewsCard({ article } ) {
       {makeImage()}
       <section className='article-text'>
         <h2
-          className='news-card-title'>
+          className='nc-title'>
           {article.headline.main}
         </h2>
         <h4>{article.byline.original}</h4>
         <h4>
           Published: <time>{new Date(article.pub_date).toDateString()}</time>
-          </h4>
-        <p>{summary}</p>
+        </h4>
+        <p data-testid='nc-summary'>{getBestSummary()}</p>
       </section>
     </Link>
   )
