3ac21aa0e2371d1d0850aac6f062d7d8065bab60
Matthew Lane <themplane@gmail.com>
2020-11-09 08:49:30 -0800

    Finish test Suit for NewsFeed.

--- src/Tests/NewsFeed.test.js
+++ src/Tests/NewsFeed.test.js
@@ -1,5 +1,6 @@
 import NewsFeed from '../Components/NewsFeed/NewsFeed.js';
 import { render, screen, waitFor } from '@testing-library/react';
+import { render as render2 } from 'react-dom'
 import { MemoryRouter } from "react-router-dom";
 import '@testing-library/jest-dom';
 import { timesAPI } from '../api-requests.js';
@@ -64,7 +65,7 @@ describe( 'NewsFeed', () => {
       expect(timesAPI.getArticles).toBeCalledWith('debug', 0);
     });
 
-    it( 'should render correctly without crashing', async () => {
+    it( 'should render an error message if the fetch fails', async () => {
 
       render(
         <MemoryRouter>
@@ -78,6 +79,76 @@ describe( 'NewsFeed', () => {
       )
 
       expect(screen.getByText('Looks like something went wrong! Refresh the page to try again.')).toBeInTheDocument();
+    });
+
+    it( 'should remove any duplicate articles', async () => {
+
+      timesAPI.getArticles = jest.fn().mockResolvedValue({
+        response: {
+          docs:[ mockArticle, mockArticle ],
+          meta: {
+            hits: 100
+          }
+        }
+      })
+
+      render(
+        <MemoryRouter>
+          <NewsFeed
+            currentPage={1}
+            isError={false}
+            query={'debug'}
+            setAppState={mockSetAppState}
+          />
+        </MemoryRouter>
+      )
+
+      await waitFor( () => expect(screen.getAllByTestId('news-card').length).toEqual(1) );
+    })
+
+    it( 'should reload when currentPage or the query props are updated ', async () => {
+
+      let node = document.createElement('div');
+      let component = render2(
+        <NewsFeed
+          currentPage={1}
+          isError={false}
+          query={'debug'}
+          setAppState={mockSetAppState}
+        />,
+        node
+      )
+
+      await waitFor( () => expect(component.state.articles.length).toEqual(1) );
+      expect(mockSetAppState).toBeCalledTimes(2);
+      expect(timesAPI.getArticles).toBeCalledTimes(1);
+
+      component = render2(
+        <NewsFeed
+          currentPage={2}
+          isError={false}
+          query={'debug'}
+          setAppState={mockSetAppState}
+        />,
+      node
+      )
+
+      await waitFor( () => expect(component.state.currentPage).toEqual(2) );
+      await waitFor( () => expect(mockSetAppState).toBeCalledTimes(4) );
+      expect(timesAPI.getArticles).toBeCalledTimes(2);
+
+      component = render2(
+        <NewsFeed
+          currentPage={2}
+          isError={false}
+          query={'foo'}
+          setAppState={mockSetAppState}
+        />,
+      node
+      )
+
+      await waitFor( () => expect(mockSetAppState).toBeCalledTimes(6) );
+      expect(timesAPI.getArticles).toBeCalledTimes(3);
     });
   });
 });
