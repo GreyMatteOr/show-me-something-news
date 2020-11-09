9d09a1751694f4f15cfecfd2228707aee483650f
Matthew Lane <themplane@gmail.com>
2020-11-09 11:17:19 -0800

    Update tests for API.

--- src/Tests/api.test.js
+++ src/Tests/api.test.js
@@ -4,39 +4,54 @@ describe( 'api helper functions', () => {
 
   describe( 'parseValues', () => {
 
-    it( 'should return a String if a string is passed in', () => {
+    it( 'should return an array with a String if a string is passed in', () => {
 
-      expect(wordAPI.parseValues('foo')).toEqual('foo');
+      expect(wordAPI.parseValues('foobar')).toEqual(['foobar']);
+    });
+
+    it( 'should split string up into words', () => {
+
+      expect(wordAPI.parseValues('foobar doobar')).toEqual(['foobar', 'doobar']);
+    });
+
+    it( 'should remove all words that are less than 3 letters', () => {
+
+      expect(wordAPI.parseValues('foo bar doobar')).toEqual(['doobar']);
+    });
+
+    it( 'should remove all forbidden words', () => {
+
+      expect(wordAPI.parseValues('noud noun adject article')).toEqual(['noud', 'adject']);
     });
 
     it( 'should return an array of strings if an Array is passed in', () => {
 
-      let test = [undefined, null, 1, 'foo', () => {}, {}];
-      let results = ['undefined', 'null', '1', 'foo', '()', '=>', '{}', '[object', 'Object]'];
-      expect(wordAPI.parseValues(test)).toEqual(results)
+      let test = [undefined, null, 1, 'foobar', () => {}, {}];
+      let results = ['foobar'];
+      expect(wordAPI.parseValues(test)).toEqual(results);
     });
 
     it( 'should return an array of the values if an object is passed in', () => {
 
-      let test = {1: 'foo', 2: 'bar', 3: 'debug'};
-      let results = ['foo', 'bar', 'debug'];
-      expect(wordAPI.parseValues(test)).toEqual(results)
+      let test = {1: 'fool', 2: 'barn', 3: 'debug'};
+      let results = ['fool', 'barn', 'debug'];
+      expect(wordAPI.parseValues(test)).toEqual(results);
     });
 
     it( 'should recurse over nested objects', () => {
 
       let test = {
-        'one': {1: 'foo', 2: 'bar', 3: 'debug'},
+        'one': {1: 'fool', 2: 'barn', 3: 'debug'},
         'two': {
           'iii': {
             1: 'in',
-            2: 'too',
+            2: 'took',
             3: 'deep'
           }
         },
         'three': [undefined, null, 1, 'foo', () => {}, {}]
       }
-      let results = ['foo', 'bar', 'debug', 'in', 'too', 'deep', 'undefined', 'null', '1', 'foo', '()', '=>', '{}', '[object', 'Object]'];
+      let results = ['fool', 'barn', 'debug', 'took', 'deep'];
       expect(wordAPI.parseValues(test)).toEqual(results)
     });
   });
@@ -44,7 +59,7 @@ describe( 'api helper functions', () => {
   describe( 'getPhrase', () => {
 
     beforeEach( () => {
-      wordAPI.getRandomWord = jest.fn().mockResolvedValue({1:'foo'});
+      wordAPI.getRandomWord = jest.fn().mockResolvedValue({1:'fool'});
     })
 
     it( 'should call `getRandomWord` at least once', async () => {
@@ -56,25 +71,25 @@ describe( 'api helper functions', () => {
     it( 'should return an array of words', async () => {
 
       let test = await wordAPI.getPhrase(1, 1);
-      expect(test).toEqual(['foo']);
+      expect(test).toEqual(['fool']);
     });
 
     it( 'should work for any number of words', async () => {
 
       let test = await wordAPI.getPhrase(1, 1);
-      expect(test).toEqual(['foo']);
+      expect(test).toEqual(['fool']);
 
       test = await wordAPI.getPhrase(2, 2);
-      expect(test).toEqual(['foo', 'foo']);
+      expect(test).toEqual(['fool', 'fool']);
 
       test = await wordAPI.getPhrase(3, 3);
-      expect(test).toEqual(['foo', 'foo', 'foo']);
+      expect(test).toEqual(['fool', 'fool', 'fool']);
 
       test = await wordAPI.getPhrase(4, 4);
-      expect(test).toEqual(['foo', 'foo', 'foo', 'foo']);
+      expect(test).toEqual(['fool', 'fool', 'fool', 'fool']);
 
       test = await wordAPI.getPhrase(5, 5);
-      expect(test).toEqual(['foo', 'foo', 'foo', 'foo', 'foo']);
+      expect(test).toEqual(['fool', 'fool', 'fool', 'fool', 'fool']);
 
     });
 
@@ -82,13 +97,13 @@ describe( 'api helper functions', () => {
 
       wordAPI.getRandomWord = jest.fn().mockResolvedValue(
         {
-          1:'foo',
-          2: 'bar',
+          1:'fool',
+          2: 'barn',
           3: {
             4: 'debug',
-            5: ['loo', 'baz', 'snup'],
+            5: ['loop', 'baz', 'snup'],
             6: {
-              7: 'can',
+              7: 'cant',
               8: 'I',
               9: 'do',
               10: 'it?'
@@ -97,7 +112,7 @@ describe( 'api helper functions', () => {
         }
       );
 
-      let correct = ['foo', 'bar', 'debug', 'loo', 'baz', 'snup', 'can', 'I', 'do', 'it?'];
+      let correct = ['fool', 'barn', 'debug', 'loop', 'snup', 'cant'];
 
       for (let i = 0; i < 20; i++) {
         let result = await wordAPI.getPhrase(5, 5);
